285c813dcc7617107a13b82f23711b8da95a8056
Andrewtho5942 <andrewtho5942@gmail.com>
2024-08-08 13:35:19 -0500

    finsih ros2 control simulations

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -11,8 +11,9 @@ from launch_ros.actions import Node
 # TO RUN THIS SCRIPT: ros2 launch AROBO7 launch_sim.launch.py
 # with maze.world: ros2 launch AROBO7 launch_sim.launch.py world:=./src/AROBO7/worlds/maze.world
 
-#Run keyboard control command:
-#ros2 run teleop_twist_keyboard teleop_twist_keyboard
+# Run keyboard control command: ros2 run teleop_twist_keyboard teleop_twist_keyboard
+# with remapped topic: ros2 run teleop_twist_keyboard teleop_twist_keyboard --ros-args -r /cmd_vel:=/diff_cont/cmd_vel_unstamped
+
 def generate_launch_description():
 
     package_name = 'AROBO7'
@@ -25,11 +26,14 @@ def generate_launch_description():
         )]), launch_arguments={'use_sim_time': 'true'}.items()
     )
 
+    gazebo_params_file = os.path.join(get_package_share_directory(package_name), 'config', 'gazebo_params.yaml')
+
     #include the gazebo launch file
     #ros2 launch gazebo_ros gazebo.launch.py extra_gazebo_args:="--verbose"
     gazebo = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([os.path.join(
             get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
+            launch_arguments={'extra+gazebo_args': '--ros-args --params-file ' + gazebo_params_file}.items()
     )   
 
   
@@ -52,7 +56,19 @@ def generate_launch_description():
                    '-z', '0'],
                    output='screen')
     
+    diff_drive_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["diff_cont"]
+    )
+
+    joint_broad_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_broad"]
+    )
+
     #Launch them all!
     return LaunchDescription([
-        rsp, gazebo, spawn_entity # teleop
+        rsp, gazebo, spawn_entity, diff_drive_spawner, joint_broad_spawner
     ])
