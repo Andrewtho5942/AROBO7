c5b63e0a9b1ceb4153dc4b34787e19d866cc6f9e
Andrewtho5942 <andrewtho5942@gmail.com>
2024-08-08 12:40:51 -0500

    update typo in controller.yaml path

--- launch/rsp.launch.py
+++ launch/rsp.launch.py
@@ -18,7 +18,7 @@ ros2 run rplidar_ros rplidar_composition --ros-args --log-level DEBUG --remap se
 
 """
 
-#Camera launch command
+#Camera launch command (broken)
 """
 ros2 run v4l2_camera v4l2_camera_node --ros-args -p image_size:="[640,480]" -p camera_frame_id:=camera_link_optical
 
